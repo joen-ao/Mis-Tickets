c9dee6d88caf0c687b29d314d7ea93c8b53644a3
joen anaya <anayajoen@gmail.com>
2024-06-22 14:05:31 -0500

    tercer commit

--- src/views/Profile/components/MyInfo/index.jsx
+++ src/views/Profile/components/MyInfo/index.jsx
@@ -1,8 +1,57 @@
+import { useForm } from "react-hook-form";
+import styles from './Myinfo.module.css';
+import { useEffect } from "react";
+
+const USER_DATA = 'userData';
+
 const MyInfo =()=>{
+
+    const {handleSubmit, register, formState: {errors}, setValue} = useForm();
+
+
+    useEffect(()=>{
+        try {
+            const userData = JSON.parse( localStorage.getItem(USER_DATA))||{} ;
+
+            setValue('name,' , userData.name);
+            setValue('email,' , userData.email);
+            setValue('age,' , userData.age);
+        } catch (error) {
+            console.error(error);
+        }
+    },[])
+
+
+    const handleFormSubmit = (data) =>{
+        try {
+            localStorage.setItem(USER_DATA, JSON.stringify(data)); //guardar la data en el local storage
+            alert('Usuario actualizado');
+        } catch (error) {
+            alert('ha ocurrido un error');
+        }
+
+    };
+
+    console.log(errors);
+
     return(
-        <div>
-            MyInfo
-        </div>
+        <form onSubmit={handleSubmit(handleFormSubmit)} className={styles.form}>
+            <label className={styles.label}>
+                Name
+                <input {...register('name', {required: true, minLength:1, maxLength: 120 })} className={styles.input} />
+            </label>
+            <label className={styles.label}>
+                Email
+                <input {...register('email', {required: true, minLength:1, maxLength: 120})} className={styles.input} />
+            </label>
+            <label className={styles.label}>
+                Age
+                <input {...register('age', {required: true, min: 18, max:100, valueAsNumber: true})}
+                 className={styles.input}
+                  type="number" />
+            </label>
+            <button type="submit" className={styles.submitButton}>Save</button>
+        </form>
     )
 };
 export default MyInfo;
--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -1,9 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import styles from './Profile.module.css';
 
 const Profile = ()=>{
+
+    const {pathname} = useLocation();
+    const navigate = useNavigate();
+    const handleTabClick = (path) =>{
+        navigate(`/profile/${path}`); //useNavigate para navegar de una pagina a otra
+    }
+
     return(
         <div>
-            Profile
+            <Link to='/' className={styles.homeLink} >Inicio</Link>
+            <div className={styles.tabsContainer}>
+                <span className={`${pathname.includes('my-info') ? styles.active: '' } ${styles.tab} `}
+                onClick={()=> handleTabClick('my-info') }
+                style={{marginRight : 8}}
+                >
+                    Mi informacion
+                </span>
+                <span className={`${pathname.includes('liked-events') ? styles.active: '' } ${styles.tab} `} 
+                onClick={()=> handleTabClick('liked-events') }
+                >
+                    Eventos favoritos
+                </span>
+            </div>
             <Outlet/> {/*componente para crear rutas anidadas*/ }
         </div>
     )
